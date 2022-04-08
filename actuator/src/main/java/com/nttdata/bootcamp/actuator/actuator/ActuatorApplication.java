02a8e200b0f5c91850919477917f4314c9899b14
Miporti <miportixt@gmail.com>
2022-04-08 11:02:38 +0200

    Proyectos__SpringCloud

---
+++ actuator/src/main/java/com/nttdata/bootcamp/actuator/actuator/ActuatorApplication.java
@@ -0,0 +1,16 @@
+package com.nttdata.bootcamp.actuator.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
+@SpringBootApplication
+public class ActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorApplication.class, args);
+	}
+
+}
